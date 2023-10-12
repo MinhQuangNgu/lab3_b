8f56f104928dc4d3743dccc16b7f1ee780430948
Minh Quang <quangminhnguyen265@gmail.com>
2023-10-12 17:28:35 +0700

    updating

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -8,7 +8,8 @@ class ProductController{
             let product = await Product.find();
             const category = await Category.find();
             const brand = await Brand.find();
-            let products = product.filter(item => {
+            let products = [];
+            product.forEach(item => {
                 let categoryTemp = {};
                 let brandTemp = {};
                 category.forEach(c => {
@@ -21,17 +22,18 @@ class ProductController{
                         brandTemp = b;
                     }
                 })
-                if(item.title.toLowerCase().includes(search.toLowerCase())){
-                    return {
+                if(item?.title?.toLowerCase()?.includes(search.toLowerCase())){
+                    products.push({
                         ...item._doc,
                         category:categoryTemp,
                         brand:brandTemp
-                    }
+                    });
                 }
             });
             return res.status(200).json({products:products});
         }
         catch(err){
+            console.log(err)
             return res.status(500).json({
                 message:err.toString()
             })
@@ -93,6 +95,27 @@ class ProductController{
             return res.status(200).json({products:products});
         }
         catch(err){
+            return res.status(500).json({
+                message:err.toString()
+            })
+        }
+    }
+
+    async createNewProduct(req,res){
+        try{
+            const {title,price,discountPercentage,rating,stock,brand,category,description} = req.body;
+            console.log({
+                title,price,discountPercentage,rating,stock,brand,category,description
+            })
+            const product = await Product.find().sort({ id: 'desc' });
+            const newProduct = new Product({
+                title,price,discountPercentage,rating,stock,brand,category,description
+            })
+            await newProduct.save();
+            return res.status(200).json({message:"Tạo mới thành công"});
+        }
+        catch(err){
+            console.log(err);
             return res.status(500).json({
                 message:err.toString()
             })
--- src/router/product.js
+++ src/router/product.js
@@ -2,9 +2,10 @@ const productController = require('../controller/productController');
 
 const router = require('express').Router();
 
-router.get('/',productController.getAllProduct);
 router.get('/brands',productController.getAllBrand);
+router.post('/create',productController.createNewProduct);
 router.get('/categories',productController.getAllCategory);
 router.get('/brands/:id',productController.getAllProductBrand);
+router.get('/',productController.getAllProduct);
 
 module.exports = router;
